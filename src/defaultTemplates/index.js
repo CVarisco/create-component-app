3ebd44da0c3851a543d5f23464149f0737133e8c
Christian Varisco <vari.chri1993@gmail.com>
2018-06-28 16:06:58 +0200

    Add prettier support on template default generation

--- src/defaultTemplates/index.js
+++ src/defaultTemplates/index.js
@@ -1,3 +1,4 @@
+import prettier from 'prettier'
 import generateClassComponent from './js/class.template'
 import generateFunctionalComponent from './js/functional.template'
 import generateTestTemplate from './js/test.template'
@@ -11,8 +12,16 @@ const types = {
   pure: generateClassComponent,
 }
 
-function generateComponentTemplate(type, name, options = {}) {
-  return types[type](name, type, options)
+async function generateComponentTemplate(type, name, options = {}) {
+  const template = types[type](name, type, options)
+
+  if (options.prettierConfig) {
+    const prettierConf = await prettier.resolveConfig(process.cwd())
+    const formatted = prettier.format(template, { ...prettierConf, parser: 'babylon' })
+    return formatted
+  }
+
+  return template
 }
 
 export {
